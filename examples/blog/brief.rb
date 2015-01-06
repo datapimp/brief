9fa9a347ef6352f28c2504a8f41bc73eb039b6a0
Jonathan Soeder <jonathan.soeder@gmail.com>
2015-01-06 00:17:57 -0600

    providing commentary in the example

--- examples/blog/brief.rb
+++ examples/blog/brief.rb
@@ -1,13 +1,20 @@
+# configuration options for this briefcase
 config do
   set(:models_path => Pathname(__FILE__).parent.join("models"))
 end
 
+# define a Post model
 define("Post") do
+
+  # the post model will have YAML frontmatter
+  # with values for 'status' and 'date'
   meta do
     status
     date DateTime, :default => lambda {|post, attr| post.document.created_at }
   end
 
+  # the post model will have a 'title' method which returns the text
+  # from the first h1 heading
   content do
     title "h1"
     has_many :subheadings, "h2"
@@ -15,17 +22,27 @@ define("Post") do
 
   helpers do
     def publish(options={})
-       puts "Publishing a post"
+
     end
   end
 
+  # Whenever we call post.save() and the status attribute changes
+  # from draft to published, do something with the model
   on_status_change(:from => "draft", :to => "published") do |model|
     # Do Something
     # mail_service.send_html_email_campaign(model.to_html)
   end
 end
 
-# brief publish posts /path/to/*.html.md
+# this creates a custom command in the brief CLI tool
+#
+# so when you run:
+#
+#   brief publish posts /path/to/*.html.md.
+#
+# the brief CLI will find models for the post files you reference,
+# and call whatever methods you want.
+
 action "publish posts" do |briefcase, models, options|
 
   say "== Publishing #{ models.length } posts"
@@ -34,3 +51,4 @@ action "publish posts" do |briefcase, models, options|
     post.publish()
   end
 end
+
