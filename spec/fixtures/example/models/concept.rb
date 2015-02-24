278cb58db4bc92d9423fbff058ef62049420d486
Jonathan Soeder <jonathan.soeder@gmail.com>
2015-02-24 02:32:29 -0600

    improvements to test suite

---
+++ spec/fixtures/example/models/concept.rb
@@ -0,0 +1,14 @@
+class Brief::Concept
+  include Brief::Model
+
+  meta do
+    title
+    needle
+    subheading
+    status String, :in => %w(draft published)
+  end
+
+  content do
+    title "h1:first-of-type"
+  end
+end
