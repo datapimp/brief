86445c5947ab446e6390a23b9cba8cd516f83850
Jonathan Soeder <jonathan.soeder@gmail.com>
2015-03-19 22:51:03 -0500

    new model for blueprint app

---
+++ apps/blueprint/models/roadmap.rb
@@ -0,0 +1,14 @@
+class Brief::Apps::Blueprint::Roadmap
+  include Brief::Model
+
+  meta do
+    title
+    subheading
+  end
+
+  content do
+    define_section "Milestones" do
+      each("h2").has(:title => "h2", :due_date => "li:first-of-type", :paragraph => "p:first-of-type")
+    end
+  end
+end
