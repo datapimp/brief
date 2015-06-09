41e693777da18792a6e1fea685d5c360e6f0b8f1
Jonathan Soeder <jonathan.soeder@gmail.com>
2015-06-09 14:52:07 -0500

    adding some commentary for the write command

--- lib/brief/cli/write.rb
+++ lib/brief/cli/write.rb
@@ -2,6 +2,9 @@ command 'write' do |c|
   c.syntax = 'brief write MODEL_TYPE [OPTIONS]'
   c.description = 'Create a new document for a given model type'
 
+  # TODO
+  # We could potential query the available model classes we are aware of
+  # and determine which CLI arguments those model classes may ask for.
   c.action do |args, options|
     schema_map = Brief.case(true).schema_map(true)
     type_alias = args.first
@@ -10,6 +13,17 @@ command 'write' do |c|
       raise "Unknown model type: #{ type_alias }. Available types are: #{ schema_map.keys.join(',') }"
     end
 
+    # TODO
+    #
+    # We need to determine the initial content that gets put into the editor.
+    #
+    # Our options are:
+    #
+    # - use an example from the model, if one exists
+    # - deduce the content to use based on some combination of one or more of the items below:
+    #   - specifics of the model
+    #   - the state of the documents that exist for that model already
+    #   - the arguments from the CLI
     default_example = "---\ntype:#{type_alias}\n---\n\n# Enter some content"
 
     content = ask_editor(model_class.to_mash.example || default_example)
