7d4f916196b07b6f326898bb2f12b9710f535865
Jonathan Soeder <jonathan.soeder@gmail.com>
2015-06-26 12:48:26 -0500

    bugfixing the run command

--- lib/brief/cli/run.rb
+++ lib/brief/cli/run.rb
@@ -2,10 +2,14 @@ command "run command" do |c|
   c.syntax = 'brief run FILE'
   c.description = 'run a script in the context of the briefcase'
 
+  c.option '--root PATH', String, 'The briefcase root'
+
   c.action do |args, options|
+    options.default(root: Pathname(args.first || Brief.pwd))
+
     command = args.first.to_s.to_sym
 
-    bc = Brief.case
+    bc = Brief.case = Brief::Briefcase.new(root: Pathname(args.first || options.root))
     bc = bc.call if bc.respond_to?(:call)
 
     if !Brief.commands[command.to_sym]
--- lib/brief/version.rb
+++ lib/brief/version.rb
@@ -1,3 +1,3 @@
 module Brief
-  VERSION = '1.15.2'
+  VERSION = '1.15.3'
 end
