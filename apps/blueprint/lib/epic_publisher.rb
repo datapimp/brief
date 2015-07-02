100695fc140146e1dcd794aff7a27b762b6bac77
Jonathan Soeder <jonathan.soeder@gmail.com>
2015-07-02 11:26:36 -0500

    adding publishing capabilities to blueprint app epic

---
+++ apps/blueprint/lib/epic_publisher.rb
@@ -0,0 +1,28 @@
+# The BlueprintEpicPublisher
+# is an adapter class which will route
+# the epic to an API integration capable of
+# publishign the epic, such as pivotal tracker or github
+class BlueprintEpicPublisher
+  def self.publish(epic, options={})
+    via = (options.fetch(:via, :github) || :github)
+
+    if respond_to?("publish_via_#{via}")
+      send("publish_via_#{via}", epic,options)
+    else
+      raise "Invalid publishing source. Need to implement publish_via_#{via} method"
+    end
+  end
+
+  # Epics get published to Pivotal
+  # directly as epics, since Epics have first class support in pivotal
+  def self.publish_via_pivotal(epic, options={})
+    raise 'This is not implemented in the app yet.  You can implement it on your own by implementing BlueprintEpicPublisher.publish_via_pivotal'
+  end
+
+  # Epics get published to Github
+  # as issues, and then get labeled as epics.
+  # epics will reference other issues.
+  def self.publish_via_github(epic, options={})
+    raise 'This is not implemented in the app yet.  You can implement it on your own by implementing BlueprintEpicPublisher.publish_via_github'
+  end
+end
--- apps/blueprint/models/epic.rb
+++ apps/blueprint/models/epic.rb
@@ -12,32 +12,6 @@ class Brief::Apps::Blueprint::Epic
     tags Array
   end
 
-  example <<-EOF
----
-type: epic
-status: draft
----
-
-# Epic Title
-
-Write a description for your epic.
-
-# Features
-
-## Feature Title
-
-As a **PERSONA** I would like to **BEHAVIOR** so that I can **GOAL**
-  EOF
-
-  template <<-EOF
-# <%= object.title %>
-# Features
-<% Array(object.features).each do |feature| %>
-## <%= feature.title %>
-As a **User** I would like to **Do this** so that I can **succeed**
-<% end %>
-  EOF
-
   content do
     title "h1:first-of-type"
     paragraph "p:first-of-type"
@@ -54,6 +28,24 @@ As a **User** I would like to **Do this** so that I can **succeed**
     end
   end
 
+  actions do
+    def validate
+      $brief_cli ? validate_cli : true
+    end
+
+    def estimate
+      estimate_cli if $brief_cli
+    end
+
+    def activate
+      raise 'Need to implement this on your own'
+    end
+
+    def publish
+      BlueprintEpicPublisher.publish(self, via: briefcase.settings.try(:tracking_system))
+    end
+  end
+
   helpers do
     def features
       sections.features.items.map do |item|
@@ -62,6 +54,85 @@ As a **User** I would like to **Do this** so that I can **succeed**
         item.merge(goal: item.components[2],
                    persona: item.components[0],
                    behavior: item.components[1])
+
+      end
+    end
+
+    def raw_content_for_feature(feature_heading, include_heading=true)
+      document.content_under_heading(feature_heading, include_heading)
+    end
+
+    def estimate_cli
+      new_content = ask_editor("# Enter point values next to each feature title\n\n#{estimations_yaml}")
+      parsed      = YAML.load(new_content) rescue nil
+
+      if parsed
+        estimations_yaml_path.open("w+") {|fh| fh.write(new_content) }
+      end
+    end
+
+    def estimations_yaml
+      estimations_data.to_yaml
+    end
+
+    def estimations_data
+      if estimations_yaml_path.exist?
+        YAML.load(estimations_yaml_path.read)
+      else
+        estimates = features.map(&:title).reduce({}) do |memo, feature_title|
+          memo[feature_title] = 0
+          memo
+        end
+
+        {
+          type: "epic_estimations",
+          document_path: document.path,
+          epic_title: title,
+          estimates: estimates
+        }.stringify_keys
+      end
+    end
+
+    def estimations_yaml_path
+      briefcase.data_path.join("estimations","#{title.parameterize}.yml")
+    end
+
+    # prints a validation report of the epic
+    def validation_report
+      warnings = []
+      errors = []
+
+      begin
+        features
+      rescue => e
+        errors.push "Error generating features: #{ e.message }"
+      end
+
+      if title.to_s.length == 0
+        errors.push "Missing epic title"
+      end
+
+      if project.to_s.length == 0
+        warnings.push "Missing project reference"
+      else
+        project_titles = Array((briefcase.projects.map(&:title) rescue nil))
+        warnings.push "Invalid project reference. #{ project } does not refer to a valid project. #{ project_titles }" unless project_titles.include?(project)
+      end
+
+      [warnings, errors]
+    end
+
+    def validate_cli
+      warnings, errors = validation_report
+
+      if !warnings.empty?
+        puts "== Epic Warnings. #{ document.path }".yellow
+        warnings.each {|w| puts "  -- #{ w }" }
+      end
+
+      if !errors.empty?
+        puts "== Epic Errors. #{ document.path }".red
+        errors.each {|w| puts "  -- #{ w }" }
       end
     end
   end
